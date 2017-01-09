81b065cb0c6da5b031a22949b93b3f7ce85ee081
Bill <wkernan@gmail.com>
2017-01-08 23:08:48 -0600

    changed sparkline to recharts

--- src/components/chart.js
+++ src/components/chart.js
@@ -1,19 +1,22 @@
 // functional component
 import _ from 'lodash';
 import React from 'react';
-import { Sparklines, SparklinesLine, SparklinesReferenceLine } from 'react-sparklines';
+import { LineChart, Line, YAxis, XAxis, CartesianGrid } from 'recharts';
 
 function average(data) {
-	return _.round(_.sum(data)/data.length);
+	const arr = data.map((data) => data.data);
+	return _.round(_.sum(arr)/arr.length);
 }
 
 export default (props) => {
 	return (
 		<div>
-			<Sparklines height={120} width={180} data={props.data}>
-				<SparklinesLine color={props.color} />
-				<SparklinesReferenceLine type="avg" />
-			</Sparklines>
+			<LineChart height={200} width={250} data={props.data} margin={{top:0, right:0, left:0, bottom:0}} >
+				<XAxis dataKey={props.time} />
+				<YAxis />
+				<CartesianGrid />
+				<Line type="monotone" dataKey={props.dataKey}  stroke={props.color} />
+			</LineChart>
 			<div>{average(props.data)}{props.symbol} {props.units}</div>
 		</div>
 	)
--- src/containers/weather-list.js
+++ src/containers/weather-list.js
@@ -4,19 +4,20 @@ import Chart from '../components/chart';
 import GoogleMap from '../components/google-map';
 
 class WeatherList extends Component {
+
 	renderWeather(cityData) {
 		const name = cityData.city.name;
-		const temps = cityData.list.map((weather) => weather.main.temp);
-		const pressures = cityData.list.map((weather) => weather.main.pressure);
-		const humidities = cityData.list.map((weather) => weather.main.humidity);
+		const temps = cityData.list.map((weather) => {return {data: weather.main.temp, date: getDate(weather.dt_txt)}});
+		const pressures = cityData.list.map((weather) => {return {data: weather.main.pressure}});
+		const humidities = cityData.list.map((weather) => {return {data: weather.main.humidity}});
 		const { lon, lat } = cityData.city.coord;
 
 		return(
 			<tr key={name}>
 				<td><GoogleMap lon={lon} lat={lat} /></td>
-				<td><Chart data={temps} color="orange" units="F" symbol="&deg;" /></td>
-				<td><Chart data={pressures} color="green" units="hPa" /></td>
-				<td><Chart data={humidities} color="black" units="%" /></td>
+				<td><Chart data={temps} dataKey="data" time="date" color="#8884d8" units="F" symbol="&deg;" /></td>
+				<td><Chart data={pressures} dataKey="data" color="#82ca9d" units="hPa" /></td>
+				<td><Chart data={humidities} dataKey="data" color="#ffc658" units="%" /></td>
 			</tr>
 		)
 	}
@@ -38,6 +39,13 @@ class WeatherList extends Component {
 			</table>
 		);
 	}
+}
+
+function getDate(date) {
+	const d = new Date(date);
+	const day = d.getDay();
+	const weekday = ['S','M','T','W','Th','F','Sa'];
+	return weekday[day];
 }
 
 function mapStateToProps({ weather }) {
